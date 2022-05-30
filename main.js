4e5a7c11dad805b3161ffa5f5fb4b8570fee4311
kanchankolte <105196286+kanchankolte@users.noreply.github.com>
2022-05-30 16:43:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    function newimage ()
+    {
+        fabric.iImage.fromURL(BirthdayImage.jpg, function(Img){
+            block_image_object=Img;
+            block-image_object.scaleTowidth(700);
+            block_image_object.scaleToheight(510);
+            block_image-objectset({
+                top:0,
+                left:0
+            })
+            canvas.add(block_image_object);
+        })
+    }	
+	
+}
+
+function playSound(){
+	function playground(){
+        x.play();
+    }
+}
